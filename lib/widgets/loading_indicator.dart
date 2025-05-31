9cfd283958d4829087295836300e21a84c9ca2a8
YuuRiLee <yurilee9696@gmail.com>
2025-05-31 15:00:16 +0900

    feat: error, loading 추가

---
+++ lib/widgets/loading_indicator.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../theme/app_colors.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  final String? message;
+  final double size;
+
+  const LoadingIndicator({Key? key, this.message, this.size = 24.0})
+    : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: size,
+            height: size,
+            child: CircularProgressIndicator(
+              strokeWidth: 2.0,
+              valueColor: AlwaysStoppedAnimation<Color>(AppColors.navy),
+            ),
+          ),
+          if (message != null) ...[
+            const SizedBox(height: 16),
+            Text(
+              message!,
+              style: Theme.of(
+                context,
+              ).textTheme.bodyMedium?.copyWith(color: AppColors.gray70),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}
