7ef3c4ccf6ebe2b9ff090cc44835365472ea6509
wookgenie <wookoding@gmail.com>
2025-06-20 00:35:54 +0900

    매핑 오류 수정

--- lib/utils/stadium_mapping.dart
+++ lib/utils/stadium_mapping.dart
@@ -4,16 +4,16 @@ class StadiumMapping {
   static String? getBestStadiumId(String stadiumName, String homeTeam) {
     // 홈팀 기반 경기장 매핑
     const teamStadiumMapping = {
-      '두산': 'jamsil',      // 잠실야구장
-      'LG': 'jamsil',        // 잠실야구장
-      '키움': 'gocheok',     // 고척스카이돔
-      'SSG': 'munhak',       // 문학경기장
-      '삼성': 'daegu',       // 대구삼성라이온즈파크
-      '한화': 'daejeon',     // 한화생명이글스파크
-      'NC': 'changwon',      // 창원NC파크
-      '롯데': 'sajik',       // 사직야구장
-      'KIA': 'gwangju',      // 광주-기아챔피언스필드
-      'KT': 'suwon',         // 수원KT위즈파크
+      '두산': 'jamsil',           // 잠실야구장
+      'LG': 'jamsil',             // 잠실야구장
+      '키움': 'gocheok',          // 고척스카이돔
+      'SSG': 'incheon_ssg',       // 인천SSG랜더스필드
+      '삼성': 'daegu_samsung',    // 대구삼성라이온즈파크
+      '한화': 'daejeon_hanwha',   // 한화생명이글스파크
+      'NC': 'changwon_nc',        // NC파크
+      '롯데': 'sajik',            // 사직야구장
+      'KIA': 'gwangju_kia',       // 광주-기아 챔피언스 필드
+      'KT': 'suwon_kt',           // 수원KT위즈파크
     };
 
     // 경기장 이름 기반 매핑
@@ -24,29 +24,33 @@ class StadiumMapping {
       '고척': 'gocheok',
       '고척스카이돔': 'gocheok',
       '고척돔': 'gocheok',
-      '문학': 'munhak',
-      '문학경기장': 'munhak',
-      'SSG랜더스필드': 'munhak',
-      '대구': 'daegu',
-      '대구삼성라이온즈파크': 'daegu',
-      '라이온즈파크': 'daegu',
-      '대전': 'daejeon',
-      '한화생명이글스파크': 'daejeon',
-      '이글스파크': 'daejeon',
-      '창원': 'changwon',
-      '창원NC파크': 'changwon',
-      'NC파크': 'changwon',
+      '문학': 'incheon_ssg',
+      '문학경기장': 'incheon_ssg',
+      'SSG랜더스필드': 'incheon_ssg',
+      '인천SSG랜더스필드': 'incheon_ssg',
+      '인천': 'incheon_ssg',
+      '대구': 'daegu_samsung',
+      '대구삼성라이온즈파크': 'daegu_samsung',
+      '라이온즈파크': 'daegu_samsung',
+      '대전': 'daejeon_hanwha',
+      '한화생명이글스파크': 'daejeon_hanwha',
+      '이글스파크': 'daejeon_hanwha',
+      '대전(신)': 'daejeon_hanwha',
+      '창원': 'changwon_nc',
+      '창원NC파크': 'changwon_nc',
+      'NC파크': 'changwon_nc',
       '사직': 'sajik',
       '사직야구장': 'sajik',
       '사직베이스볼파크': 'sajik',
-      '광주': 'gwangju',
-      '광주-기아챔피언스필드': 'gwangju',
-      '챔피언스필드': 'gwangju',
-      '기아챔피언스필드': 'gwangju',
-      '수원': 'suwon',
-      '수원KT위즈파크': 'suwon',
-      'KT위즈파크': 'suwon',
-      '위즈파크': 'suwon',
+      '광주': 'gwangju_kia',
+      '광주-기아챔피언스필드': 'gwangju_kia',
+      '광주-기아 챔피언스 필드': 'gwangju_kia',
+      '챔피언스필드': 'gwangju_kia',
+      '기아챔피언스필드': 'gwangju_kia',
+      '수원': 'suwon_kt',
+      '수원KT위즈파크': 'suwon_kt',
+      'KT위즈파크': 'suwon_kt',
+      '위즈파크': 'suwon_kt',
     };
 
     // 1. 경기장 이름으로 직접 매핑 시도
